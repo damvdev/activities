6838fd02321c86d84a09073453b789034ff4e047
Raquel Alamán <raquel.alaman@itb.cat>
2024-11-12 12:37:25 +0100

    Solució exercici 3. Closes #4

--- Program.cs
+++ Program.cs
@@ -4,10 +4,18 @@ namespace Activities {
         public static bool IsNatural(int num) {
             return num > 0;
         }
+        public static bool IsNotNatural(int num)
+        {
+            return !IsNatural(num);
+        }
         public static void Main()
         {
+            // Test IsNatural
             Console.WriteLine(IsNatural(3));
             Console.WriteLine(IsNatural(-3));
+            // Test IsNotNatural
+            Console.WriteLine(IsNotNatural(3));
+            Console.WriteLine(IsNotNatural(-3));
         }
     }
 }
